eac11ab5f3c2a443b50833b4972b0ab04a5621fe
akina42 <akina_ak@hotmail.com>
2019-02-04 23:38:36 -0200

    Add - endpoint get cadastro e modelmapper

--- src/main/java/com/iadtec/hackathon/Controller/RegisterOneController.java
+++ src/main/java/com/iadtec/hackathon/Controller/RegisterOneController.java
@@ -1,9 +1,16 @@
 package com.iadtec.hackathon.Controller;
 
+import com.iadtec.hackathon.DTO.RegisterOneDTO;
 import com.iadtec.hackathon.Service.RegisterOneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RequestMapping("/api/v1/registerOne")
 @RestController
@@ -12,4 +19,13 @@ public class RegisterOneController {
     @Autowired
     private RegisterOneService registerOneService;
 
+    public RegisterOneController() {
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<RegisterOneDTO> getRegisterOne(@PathVariable("id") Long id){
+       Optional<RegisterOneDTO> registerOneDTOOptional = registerOneService.getRegisterOne(id);
+       registerOneDTOOptional.map(registerOneDTO -> new ResponseEntity<>(registerOneDTO, HttpStatus.OK));
+       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/iadtec/hackathon/Service/RegisterOneService.java
+++ src/main/java/com/iadtec/hackathon/Service/RegisterOneService.java
@@ -1,8 +1,13 @@
 package com.iadtec.hackathon.Service;
 
+import com.iadtec.hackathon.DTO.RegisterOneDTO;
+import com.iadtec.hackathon.Entity.RegisterOne;
 import com.iadtec.hackathon.Repository.RegisterOneRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RegisterOneService {
@@ -10,7 +15,18 @@ public class RegisterOneService {
     @Autowired
     private RegisterOneRepository registerOneRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public RegisterOneService() {
     }
 
+    public Optional<RegisterOneDTO> getRegisterOne(Long id){
+        Optional<RegisterOne> registerOneOptional = registerOneRepository.findById(id);
+        registerOneOptional.map( registerOne -> {
+            RegisterOneDTO registerOneDTO = modelMapper.map(registerOne, RegisterOneDTO.class);
+            return Optional.of(registerOneDTO);
+        }).orElse(Optional.empty());
+        return Optional.empty();
+    }
 }
